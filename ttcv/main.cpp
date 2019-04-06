cd9f059379432dabc4204427cd61dc83feddacf3
Benjamin Lokash <blokash@qmo.io>
2019-04-06 14:58:59 -0400

    Initial Commit

---
+++ ttcv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ttcv
+//
+//  Created by QuantumMob on 2019-04-06.
+//  Copyright © 2019 QuantumMob. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
